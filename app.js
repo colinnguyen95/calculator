3ba2b32b70c9578dc5d93d113d4df0d9f160defc
Colin Nguyen <nguyencolin03@gmail.com>
2022-03-14 09:57:38 -0700

    added operator support, just need to work on more styling

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ let moveNextNum = false;
 let equalClicked = false;
 let deleteClicked = false;
 
-const validkeys=[ "0","1","2","3","4","5","6","7","8","9","+","-","x","%","." ];
+const validkeys=[ "0","1","2","3","4","5","6","7","8","9","." ];
+const validOperations = ["+","-","x","%"];
+const validEnterKeys = ["=","Enter"];
 
 const add = (num1, num2) => {
     console.log(num1 + num2);
@@ -63,6 +65,43 @@ const operate = (operator, num1, num2) => {
     return result;
 }
 
+const operatorFunc = (e) => {
+    const action = e.key ? e.key : e.target.textContent;
+    print.textContent += action;
+    operatorClicked = true;
+    operatorClickCount++;
+    if(operatorClickCount > 1){
+        result = operate(operator, parseInt(num1), parseInt(num2));
+        num1 = result;
+        mainScreen.textContent = num1;
+    }
+    console.log('operatorClickCount: ', operatorClickCount)
+
+    switch(action){
+        case '+':
+            operator = 'add';
+            break;
+        case '-':
+            operator = 'subtract';
+            break;
+        case 'x':
+            operator = 'multiply';
+            break;
+        case '%':
+            operator = 'divide';
+            break;
+    }
+    moveNextNum = true;
+    clearForNextNum = true;
+
+    if(equalClicked === true){
+        print.textContent = result + action;
+        equalClicked = false;
+    }
+    console.log('operator: ', operator);
+    console.log('num1 after operator delete', num1);
+}
+
 const clearFunc = () => {
     mainScreen.textContent = '';
     print.textContent = '';
@@ -99,12 +138,7 @@ const deleteFunc = () => {
         print.textContent = c;
         operatorClickCount--;
     }
-    // let mainScreenDelete = mainScreen.textContent;
-    // let printScreenDelete = print.textContent;
-    // let b = mainScreenDelete.slice(0, mainScreenDelete.length - 1);
-    // let c = printScreenDelete.slice(0, printScreenDelete.length - 1);
-    // mainScreen.textContent = b;
-    // print.textContent = c;
+    
     if(!moveNextNum){
         num1 = mainScreen.textContent
     }
@@ -137,41 +171,7 @@ numButtons.forEach(button => {
 })
 
 operateButtons.forEach(button => {
-    button.addEventListener('click', e => {
-        print.textContent += e.target.textContent;
-        operatorClicked = true;
-        operatorClickCount++;
-        if(operatorClickCount > 1){
-            result = operate(operator, parseInt(num1), parseInt(num2));
-            num1 = result;
-            mainScreen.textContent = num1;
-        }
-        console.log('operatorClickCount: ', operatorClickCount)
-
-        switch(e.target.textContent){
-            case '+':
-                operator = 'add';
-                break;
-            case '-':
-                operator = 'subtract';
-                break;
-            case 'x':
-                operator = 'multiply';
-                break;
-            case '%':
-                operator = 'divide';
-                break;
-        }
-        moveNextNum = true;
-        clearForNextNum = true;
-
-        if(equalClicked === true){
-            print.textContent = result + e.target.textContent
-            equalClicked = false;
-        }
-        console.log('operator: ', operator);
-        console.log('num1 after operator delete', num1);
-    })
+    button.addEventListener('click', operatorFunc);
 })
 
 equals.addEventListener('click', (e) => {
@@ -208,15 +208,13 @@ clear.addEventListener('click', clearFunc)
 
 deleteBtn.addEventListener('click', deleteFunc)
 
-
 document.addEventListener('keydown', (e) => {
     let key = e.key;
-    console.log(key);
+    
     if(validkeys.indexOf(key) >= 0){
         print.textContent += key;
 
         if(!moveNextNum){
-            // mainScreen.textContent += e.target.textContent;
             mainScreen.textContent += key;
             num1 = mainScreen.textContent
         }
@@ -225,13 +223,26 @@ document.addEventListener('keydown', (e) => {
             clearForNextNum = false;
         }
         if(moveNextNum) {
-            // mainScreen.textContent += e.target.textContent;
             mainScreen.textContent += key;
             num2 = mainScreen.textContent;
         }
-        console.log(e.target.textContent);
         console.log('num1: ', num1);
         console.log('num2: ', num2);
+        // console.log('test operator event', e.key);
+    }
+
+    if(validOperations.indexOf(key) >= 0){
+        operatorFunc(e);
+    }
+    if(validEnterKeys.indexOf(key) >= 0){
+        equals.click();
+    }
+    if(key === 'Backspace'){
+        deleteBtn.click();
     }
 })
+
+// document.addEventListener('keydown', e => {
+//     console.log('test e: ', e);
+// })
 
